dfbb4640880e8b2e49ec0f518b786dc7ef89176e
GloriusFy <lz.glorius@gmail.com>
2022-08-20 19:37:58 +0700

    main

--- Stock.WebAPI/Cors/Startup.cs
+++ Stock.WebAPI/Cors/Startup.cs
@@ -9,32 +9,28 @@ internal static class Startup
 
     internal static void AddCorsConfiguration(this IServiceCollection services, IConfiguration configuration)
     {
-        // var corsSettings = configuration.GetOptions<CorsSettings>();
-        //
-        // if (corsSettings == null)
-        // {
-        //     return;
-        // }
-        //
-        services.AddCors();
-        // services.AddCors(options =>
-        // {
-        //     options.AddDefaultPolicy(builder =>
-        //     {
-        //         builder
-        //             .AllowAnyMethod()
-        //             .AllowAnyHeader()
-        //             .AllowCredentials()
-        //             .SetIsOriginAllowedToAllowWildcardSubdomains()
-        //             .WithOrigins(
-        //                 corsSettings.AllowedOrigins)
-        //             .Build();
-        //     });
-        // });
+         var corsSettings = configuration.GetOptions<CorsSettings>();
+        
+ 
+         services.AddCors();
+         services.AddCors(options =>
+         {
+             options.AddDefaultPolicy(builder =>
+             {
+                 builder
+                     .AllowAnyMethod()
+                     .AllowAnyHeader()
+                     .AllowCredentials()
+                     .SetIsOriginAllowedToAllowWildcardSubdomains()
+                     .WithOrigins(
+                         corsSettings.AllowedOrigins)
+                     .Build();
+             });
+         });
     }
 
     public static void UseCorsConfiguration(this IApplicationBuilder app)
     {
-        app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+        app.UseCors();
     }
 }
